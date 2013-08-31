021a872410b4b323a5ea0b8118aabd79d5a4d32f
mpmckenna8 <mpmckenna8@me.com>
2013-08-31 16:54:49 -0700

    popupheader
    Header on them pop ups.

--- sfbikevalet-geojson.js
+++ sfbikevalet-geojson.js
@@ -2,19 +2,19 @@ var valetlocations = {
     "type": "FeatureCollection",
     
     "features": [
-                 { "type": "Feature", "id": 0, "properties": { "id": 1, "Bike Valet": "Giants", "Capacity": 200,
+                 { "type": "Feature", "id": 0, "properties": { "id": 1, "Bike_Valet": "Giants", "Capacity": 200,
                  "Dates": ["23\/8\/2013", "05\/09\/2013", "06\/09\/2013"], "Times": "1700," }, "geometry": { "type": "Point", "coordinates": [ -122.38665, 37.77936 ] } }
                  ,
-                 { "type": "Feature", "id": 1, "properties": { "id": 2, "Bike Valet": "America's Cup Marina Green",
+                 { "type": "Feature", "id": 1, "properties": { "id": 2, "Bike_Valet": "America's Cup Marina Green",
                  "Capacity": 100, "Dates": ["01\/09\/2013", "02\/09\/2013", "03\/09\/2013", "04\/09\/2013"], "Times": ["1000-1900"] }, "geometry": { "type": "Point", "coordinates": [ -122.43507, 37.80633  ] } }
                  ,
-                 { "type": "Feature", "id": 2, "properties": { "id": 3, "Bike Valet": "America's Cup Embarcadero", "Capacity": 100,
+                 { "type": "Feature", "id": 2, "properties": { "id": 3, "Bike_Valet": "America's Cup Embarcadero", "Capacity": 100,
                  "Dates": ["01\/09\/2013", "02\/09\/2013", "03\/09\/2013", "04\/09\/2013"], "Times": "1000-1900" }, "geometry": { "type": "Point", "coordinates": [ -122.399621550709114, 37.804334682534055 ] } }
                  ,
-                 { "type": "Feature", "id": 3, "properties": { "id": 4, "Bike Valet": "Metromint di Giro", "Capacity": 80,
+                 { "type": "Feature", "id": 3, "properties": { "id": 4, "Bike_Valet": "Metromint di Giro", "Capacity": 80,
                  "Dates": ["09\/02\/2013"], "Times": "1100-1630" }, "geometry": { "type": "Point", "coordinates": [ -122.401397781663391, 37.802384355311119 ] } }
                  ,
-                 { "type": "Feature", "id": 4, "properties": { "id": 5, "Bike Valet": "JP Morgan Corporate Challenge", "Capacity": 150, "Dates": ["09\/03\/2013"], "Times": "1700-2230" }, "geometry": { "type": "Point", "coordinates": [ -122.388659372905778, 37.774641900381418 ] } }
+                 { "type": "Feature", "id": 4, "properties": { "id": 5, "Bike_Valet": "JP Morgan Corporate Challenge", "Capacity": 150, "Dates": ["09\/03\/2013"], "Times": "1700-2230" }, "geometry": { "type": "Point", "coordinates": [ -122.388659372905778, 37.774641900381418 ] } }
                  
                  ]
 }
