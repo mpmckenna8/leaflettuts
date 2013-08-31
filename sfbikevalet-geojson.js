c3d1b0b3e475d21b6ab8a4bc72a50f6cf0ac3bca
mpmckenna8 <mpmckenna8@me.com>
2013-08-30 19:18:59 -0700

    valetmaprevamp
    SFBC icons and new basemap.

--- sfbikevalet-geojson.js
+++ sfbikevalet-geojson.js
@@ -3,10 +3,10 @@ var valetlocations = {
     
     "features": [
                  { "type": "Feature", "id": 0, "properties": { "id": 1, "Bike Valet": "Giants", "Capacity": 200,
-                 "Dates": ["23\/8\/2013", "05\/09\/2013", "06\/09\/2013"], "Times": "1700," }, "geometry": { "type": "Point", "coordinates": [ -122.388882055788812, 37.778022651545839 ] } }
+                 "Dates": ["23\/8\/2013", "05\/09\/2013", "06\/09\/2013"], "Times": "1700," }, "geometry": { "type": "Point", "coordinates": [ -122.38665, 37.77936 ] } }
                  ,
                  { "type": "Feature", "id": 1, "properties": { "id": 2, "Bike Valet": "America's Cup Marina Green",
-                 "Capacity": 100, "Dates": ["01\/09\/2013", "02\/09\/2013", "03\/09\/2013", "04\/09\/2013"], "Times": ["1000-1900"] }, "geometry": { "type": "Point", "coordinates": [ -122.421318975774909, 37.807891490674393 ] } }
+                 "Capacity": 100, "Dates": ["01\/09\/2013", "02\/09\/2013", "03\/09\/2013", "04\/09\/2013"], "Times": ["1000-1900"] }, "geometry": { "type": "Point", "coordinates": [ -122.43507, 37.80633  ] } }
                  ,
                  { "type": "Feature", "id": 2, "properties": { "id": 3, "Bike Valet": "America's Cup Embarcadero", "Capacity": 100,
                  "Dates": ["01\/09\/2013", "02\/09\/2013", "03\/09\/2013", "04\/09\/2013"], "Times": "1000-1900" }, "geometry": { "type": "Point", "coordinates": [ -122.399621550709114, 37.804334682534055 ] } }
