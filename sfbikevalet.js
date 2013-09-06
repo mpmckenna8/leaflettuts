5d7d59154fb75ccc0ab612eb17159d17e03df1fe
mpmckenna8 <mpmckenna8@me.com>
2013-09-06 08:32:55 -0700

    modularizingabit
    making my bike valet map a little more modular… I think I'm using that
    right.

---
+++ sfbikevalet.js
@@ -0,0 +1,47 @@
+            var valeticon = L.icon({
+                                   iconUrl :'SFbikeValet-mapicon.png',
+                                   iconSize: [32,37],
+                                   iconAnchor: [0,0],
+                                   popupAnchor: [0, 0]
+                                   });
+            
+            function initialize() {
+                var layers = ["toner"];
+                
+                var layer = "toner";
+                var map = new L.Map(layer, {
+                                    center: new L.LatLng(37.75, -122.45),
+                                    zoom: 12
+                                    });
+                map.addLayer(new L.StamenTileLayer(layer));
+                
+                function onEachFeature(feature, layer) {
+                    // does this feature have a property named popupContent?
+                   
+                    if (feature.properties && feature.properties.Dates[0]) {
+                      var popUP = [];
+                        for(i=0; i<feature.properties.Dates.length; i++){
+                            popUP[i] = feature.properties.Dates[i];
+                        }
+                           layer.bindPopup("<h4>" + feature.properties.Bike_Valet +"</h4><p>Come Park your bike on: </p>" + String(popUP));
+                    }
+                }
+                
+                L.geoJson(valetlocations, {
+                        
+                          pointToLayer: function (feature, latlng){
+                          return L.marker(latlng, {icon: valeticon});
+                          },
+                           onEachFeature: onEachFeature
+                          }).addTo(map)
+                //The popup I had the easyn non-dynamic way.
+                ///.bindPopup("<p>Come Park your bike on need to figure out how to get right date from json array.</p>");
+                function onMapClick(e) {
+                    alert("You clicked the map at " + e.latlng);
+                }
+                
+                map.on('click', onMapClick);
+                
+            }
+            
+            
