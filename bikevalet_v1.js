db67cd8f55cdceaa11dca854014f2b212bd41ad8
Matthew McKenna <mpmckenna8@me.com>
2014-03-26 20:35:58 -0700

    some fixes i guess

--- bikevalet_v1.js
+++ bikevalet_v1.js
@@ -1,34 +1,37 @@
-                    var valeticon = L.Icon.extend({
+
+var valeticon = L.Icon.extend({
             	options: {
-                                   iconSize: [32,37],
-                                   iconAnchor: [15,18.5],
-                                   popupAnchor: [0, 0]
-                                   }
-                                   });
-            var sfbikeicon = new valeticon({iconUrl :'SFbikeValet-mapicon.png'})
-       		var wPicon = new valeticon({iconUrl :'bikeparking.svg' })
-                
+                       iconSize: [32,37],
+                       iconAnchor: [15,18.5],
+                       popupAnchor: [0, 0]
+                        }
+});
+
+    var sfbikeicon = new valeticon({iconUrl :'SFbikeValet-mapicon.png'});
+
+		var wPicon = new valeticon({iconUrl :'bikeparking.svg' });
+
             var layer = "toner";
             var map = new L.Map(layer, {
     	            center: new L.LatLng(37.75, -122.45),
                      zoom: 12
                                         });
-                
+
             map.addLayer(new L.StamenTileLayer(layer));
-                
-                function onEachFeature(feature, layer) {
-                   
-                    if (feature.properties && feature.properties.Dates[0]) {
+
+    function onEachFeature(feature, layer) {
+
+        if (feature.properties && feature.properties.Dates[0]) {
                       var popUP = [];
-                        for(i=0; i<feature.properties.Dates.length; i++){
-                            popUP[i] = feature.properties.Dates[i];
+              for(i=0; i<feature.properties.Dates.length; i++){
+                    popUP[i] = feature.properties.Dates[i];
                         }
-                           layer.bindPopup("<h4>" + feature.properties.Bike_Valet +"</h4><p>Come Park your bike on: </p>" + String(popUP));
+          layer.bindPopup("<h4>" + feature.properties.Bike_Valet +"</h4><p>Come Park your bike on: </p>" + String(popUP));
                     }
                 }
-                
+
             var geojson = L.geoJson(valetlocations, {
-                        
+
                     pointToLayer: function (feature, latlng){
                     if(feature.properties.id<100){
                     return L.marker(latlng, {icon: sfbikeicon});}
@@ -38,7 +41,7 @@
                           },
                            onEachFeature: onEachFeature
                           }).addTo(map)
-                          
+
                 linky = []
                 var options = '<option value="Select a Feature">Select a Feature</option>';
                 geojson.eachLayer(function (layer) {
@@ -47,11 +50,11 @@
 		    var coords = layer.feature.geometry.coordinates;
 			var lng = layer.feature.geometry.coordinates[0];
 			var lat = layer.feature.geometry.coordinates[1];
-			
+
 			linky.push({id: id, label: name, value: name, lat: lat, lng: lng});
-		    
+
 		    console.log(coords);
-		    
+
 
 		   //This is where I'm adding stuff to my table of clickable links.
 		    $("#featurelist table").append('<tr><td><a href="#" onclick="map._layers['+id+'].openPopup(); map.setView(['+lat+', '+lng+'], 16); return false;">'+name+'</a></td></tr>');
@@ -59,7 +62,7 @@
 			options += '<option value="' + lng + "," + lat + '", id="' + id + '">' + name + '</option>';
 		});
       	$("#zoom").html(options);
-      	
+
       	// Function call by "zoom" dropdown onSelect
 		function zoomToFeature (state, coords, id) {
 			if (state === "Select a Feature") {
@@ -71,10 +74,10 @@
 				map._layers[id].openPopup();
 			};
 		}
-		
+
 		//My Button Fuctions
              function centermap(){
-               
+
               map.setView([37.75, -122.45], 12)
-              
-                }   
+
+                }
